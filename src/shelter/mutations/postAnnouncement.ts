e717ac8ea6120056c7be77899483297011b5959d
magulloff <przemczar2001@gmail.com>
2023-03-31 17:34:49 +0200

    untested

--- src/shelter/mutations/postAnnouncement.ts
+++ src/shelter/mutations/postAnnouncement.ts
@@ -1,6 +1,17 @@
+import { useQuery } from "react-query";
+import { BACKEND_URL } from "../../backendUrl";
 import { NewAnouncement } from "../../types/newAnnouncement";
 
 export const postAnnouncement = (announcement: NewAnouncement) => {
-  //posting here
-  console.log(announcement)
+  const query = useQuery('new-announcements', () => fetch(
+    BACKEND_URL + "announcements",
+    {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(announcement)
+    }
+  ).then(res => res.json()));
+  return query
 }
--- src/shelter/queries/myPets.ts
+++ src/shelter/queries/myPets.ts
@@ -1,38 +1,16 @@
+import { useQuery } from "react-query"
+import { BACKEND_URL } from "../../backendUrl";
 import { Pet } from "../../types/pet"
 
 export const useMyPets = () => {
-  const query = {
-    isLoading: false,
-    data: [
-      {
-        name: "fifik1",
-        breed: "Best Breed",
-        species: "Best species",
-        description: "",
-        id: "1111-111-11111"
-      },
-      {
-        name: "fifik2",
-        breed: "Best Breed",
-        species: "Best species",
-        description: "",
-        id: "2111-111-11111"
-      },
-      {
-        name: "fifik3",
-        breed: "Best Breed",
-        species: "Best species",
-        description: "",
-        id: "3111-111-11111"
-      },
-      {
-        name: "fifik4",
-        breed: "Best Breed",
-        species: "Best species",
-        description: "",
-        id: "4111-111-11111"
-      },
-    ] as Pet[]
-  }
+  const query = useQuery<Pet[]>('my-pets', () => fetch(
+    BACKEND_URL + "pet",
+    {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+  ).then(res => res.json()));
   return query
 }
