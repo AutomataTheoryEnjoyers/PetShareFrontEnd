757c0698898154714c084e72cd404d815f0d119a
magulloff <przemczar2001@gmail.com>
2023-04-04 11:53:48 +0200

    fix mock menu

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
   return <>
     <h1> mock navigation </h1>
     <h4> click to go to use module </h4>
-    <h3 onClick={() => navigate("/user")}>USER</h3>
-    <h3 onClick={() => navigate("/shelter")}>SHELTER </h3>
+    <h3 onClick={() => navigate("/user/announcements")}>USER</h3>
+    <h3 onClick={() => navigate("/shelter/my-announcements")}>SHELTER </h3>
   </>;
 };
