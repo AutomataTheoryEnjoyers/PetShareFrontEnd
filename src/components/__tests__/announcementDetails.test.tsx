e4f197e9ab5267b10a69b2cc3af5c1b3c43b0e0f
Karol Kącki <karol.kacki2@gmail.com>
2023-04-21 13:27:37 +0200

    refactor

--- src/components/__tests__/announcementDetails.test.tsx
+++ src/components/__tests__/announcementDetails.test.tsx
@@ -3,30 +3,25 @@ import { AnnouncementDetailsElement } from "../announcementDetails";
 import { mockAnnouncements } from "../../mocks/mockData";
 import { renderWithRouter } from "../testUtils/functions";
 
-test("renders AnnouncementDetailsElement component", () => {
-  const { getByText } = renderWithRouter(
-    <AnnouncementDetailsElement announcement={mockAnnouncements[0]} />
-  );
+describe("AnnouncementDetailsElement", () => {
+  it("renders the announcement details", () => {
+    const { getByText } = renderWithRouter(
+      <AnnouncementDetailsElement announcement={mockAnnouncements[0]} />
+    );
 
-  expect(
-    getByText(`Created: ${mockAnnouncements[0].creationDate.toDateString()}`)
-  ).toBeInTheDocument();
-  expect(
-    getByText(
-      `Last Update: ${mockAnnouncements[0].lastUpdateDate.toDateString()}`
-    )
-  ).toBeInTheDocument();
-  expect(
-    getByText(`Status: ${mockAnnouncements[0].status}`, { exact: false })
-  ).toBeInTheDocument();
-  expect(getByText(`ID: ${mockAnnouncements[0].id}`)).toBeInTheDocument();
-  if (mockAnnouncements[0].closingDate) {
+    expect(
+      getByText(`Created: ${mockAnnouncements[0].creationDate.toDateString()}`)
+    ).toBeInTheDocument();
     expect(
       getByText(
-        `Closing date: ${mockAnnouncements[0].closingDate.toDateString()}`
+        `Last Update: ${mockAnnouncements[0].lastUpdateDate.toDateString()}`
       )
     ).toBeInTheDocument();
-  }
-  expect(getByText(mockAnnouncements[0].title)).toBeInTheDocument();
-  expect(getByText(mockAnnouncements[0].description)).toBeInTheDocument();
+    expect(
+      getByText(`Status: ${mockAnnouncements[0].status}`)
+    ).toBeInTheDocument();
+    expect(getByText(`ID: ${mockAnnouncements[0].id}`)).toBeInTheDocument();
+    expect(getByText(mockAnnouncements[0].title)).toBeInTheDocument();
+    expect(getByText(mockAnnouncements[0].description)).toBeInTheDocument();
+  });
 });
