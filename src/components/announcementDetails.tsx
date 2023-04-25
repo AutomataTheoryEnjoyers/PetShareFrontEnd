40cf9731717d06e642f7ddc212dbc7b132a86498
magulloff <przemczar2001@gmail.com>
2023-04-25 21:53:21 +0200

    naming changes

--- src/components/announcementDetails.tsx
+++ src/components/announcementDetails.tsx
@@ -14,24 +14,24 @@ type Props = {
 export const AnnouncementDetailsElement = ({ announcement, isFollowed, isShelter }: Props) => {
   const [overFollow, setOverFollow] = useState(isFollowed ?? false);
   return (
-    <AnnouncementDetailsContainer>
-      <AnnouncementDetailsContainerDates>
-        <AnnouncementDetailsContainerDatesLeft>
+    <Container>
+      <TopContainer>
+        <TopContainerLeft>
           <TextDetails>
             Created: {announcement.creationDate.toDateString()}
           </TextDetails>
           <TextDetails>
             Last Update: {announcement.lastUpdateDate.toDateString()}
           </TextDetails>
-        </AnnouncementDetailsContainerDatesLeft>
-        <AnnouncementDetailsContainerDatesRight>
+        </TopContainerLeft>
+        <TopContainerRight>
           {!isShelter && <FollowContainer
             onMouseOver={() => setOverFollow(true)}
             onMouseLeave={() => setOverFollow(false)}
             onClick={() => { /*function for following*/ }}
           ><FontAwesomeIcon icon={faHeart} style={{ transform: `scale(${overFollow ? 1.15 : 1})` }} font-size={25} color={isFollowed ? "red" : overFollow ? "red" : "black"} className="followIcon"></FontAwesomeIcon></FollowContainer>}
-        </AnnouncementDetailsContainerDatesRight>
-      </AnnouncementDetailsContainerDates>
+        </TopContainerRight>
+      </TopContainer>
       <Title>{announcement.title && announcement.title}</Title>
       <DescriptionText>
         {announcement.description && announcement.description}
@@ -40,11 +40,11 @@ export const AnnouncementDetailsElement = ({ announcement, isFollowed, isShelter
         <Separator />
         <AnnouncementIdContainer>ID: {announcement.id}</AnnouncementIdContainer>
       </BottomContainer>
-    </AnnouncementDetailsContainer>
+    </Container>
   );
 };
 
-const AnnouncementDetailsContainer = styled.div`
+const Container = styled.div`
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   border-radius: 5px;
   padding: 12px;
@@ -52,19 +52,19 @@ const AnnouncementDetailsContainer = styled.div`
   background-color: ${(props) => props.theme.colors.powderWhite};
   text-align: left;
 `;
-const AnnouncementDetailsContainerDates = styled.div`
+const TopContainer = styled.div`
   display: flex;
   flex-direction: row;
   padding-left: 5px;
   padding-right: 5px;
   justify-content: space-between;
 `;
-const AnnouncementDetailsContainerDatesLeft = styled.div`
+const TopContainerLeft = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
 `;
-const AnnouncementDetailsContainerDatesRight = styled.div`
+const TopContainerRight = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
