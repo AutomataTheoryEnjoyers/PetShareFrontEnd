438011fc40ff6b679e9896afbbc36c43830f9ba1
magulloff <przemczar2001@gmail.com>
2023-04-25 17:25:58 +0200

    add button

--- src/user/views/announcementDetails/index.tsx
+++ src/user/views/announcementDetails/index.tsx
@@ -32,11 +32,32 @@ export const AnnouncementDetails = () => {
           <div id="details">
             <AnnouncementDetailsElement announcement={currentAnnouncement} />
           </div>
+          <div id="apply-button">
+            <ApplyButton onClick={() => {/*adopt function*/ }}>Adopt!</ApplyButton>
+          </div>
         </Container>
       </AnimatedPage>
     )
   );
 };
+
+const ApplyButton = styled.div`
+  background: ${(props) => props.theme.colors.main};
+  color: #fff;
+  border: 0;
+  border-radius: 5px;
+  cursor: pointer;
+  outline: none;
+  padding: 5px;
+  font-size: 30px;
+  height: 50px;
+  font-weight: 600;
+  letter-spacing: 5px;
+  transition: 0.5s all;
+  :hover {
+    background: ${(props) => props.theme.colors.darkGreen};
+  }
+`
 
 const Container = styled.div`
   text-align: center;
@@ -50,7 +71,8 @@ const Container = styled.div`
     "image image shelter"
     "details details details"
     "details details details"
-    "user user user";
+    "user user user"
+    "apply apply apply";
 
   grid-template-columns: 1fr 1fr 1fr;
 
@@ -72,5 +94,9 @@ const Container = styled.div`
 
   #details {
     grid-area: details;
+  }
+
+  #apply-button{
+    grid-area: apply;
   }
 `;
