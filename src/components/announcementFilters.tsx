397d0dffed2b2a33287df0e95a0740581c8d9828
magulloff <przemczar2001@gmail.com>
2023-04-27 18:00:32 +0200

    basic adding layout

--- src/components/announcementFilters.tsx
+++ src/components/announcementFilters.tsx
@@ -1,3 +1,5 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 
 export type FilterState = {
@@ -20,7 +22,7 @@ export const AnnouncementFilters = ({ filters, onChange }: Props) => {
       <FieldsContainer>
         <AgeCategoryContainer>
           <CategoryTitle>Age</CategoryTitle>
-          <Input
+          <NumberInput
             maxLength={2}
             type="number"
             placeholder="min age"
@@ -28,7 +30,7 @@ export const AnnouncementFilters = ({ filters, onChange }: Props) => {
             onChange={(e) => onChange({ minAge: e.target.value })}
             required
           />
-          <Input
+          <NumberInput
             maxLength={2}
             type="number"
             placeholder="max age"
@@ -39,21 +41,62 @@ export const AnnouncementFilters = ({ filters, onChange }: Props) => {
         </AgeCategoryContainer>
         <CategoryContainer>
           <CategoryTitle>City</CategoryTitle>
+          <NewPositionContainer>
+            <Input
+              maxLength={15}
+              type="text"
+              placeholder="City name"
+              required
+            />
+            <AddButton><FontAwesomeIcon fontSize={30} icon={faPlus} /></AddButton>
+          </NewPositionContainer>
         </CategoryContainer>
         <CategoryContainer>
           <CategoryTitle>Species</CategoryTitle>
+          <NewPositionContainer>
+            <Input
+              maxLength={15}
+              type="text"
+              placeholder="Species name"
+              required
+            />
+            <AddButton><FontAwesomeIcon fontSize={30} icon={faPlus} /></AddButton>
+          </NewPositionContainer>
         </CategoryContainer>
         <CategoryContainer>
           <CategoryTitle>Breed</CategoryTitle>
+          <NewPositionContainer>
+            <Input
+              maxLength={15}
+              type="text"
+              placeholder="Breed name"
+              required
+            />
+            <AddButton><FontAwesomeIcon fontSize={30} icon={faPlus} /></AddButton>
+          </NewPositionContainer>
         </CategoryContainer>
         <CategoryContainer>
           <CategoryTitle>Shelter</CategoryTitle>
+          <NewPositionContainer>
+            <Input
+              maxLength={2}
+              type="text"
+              placeholder="Shelter name"
+              required
+            />
+            <AddButton><FontAwesomeIcon fontSize={30} icon={faPlus} /></AddButton>
+          </NewPositionContainer>
         </CategoryContainer>
       </FieldsContainer>
       <FormButton></FormButton>
     </FormContainer>
   );
 }
+
+const NewPositionContainer = styled.div`
+  display: flex;
+  gap: 3px;
+`
 
 const FormContainer = styled.div`
   display: flex;
@@ -73,19 +116,52 @@ const Input = styled.input`
   font-size: 20px;
   font-weight: 400;
   text-align: center;
+  flex: 4;
+  -webkit-appearance: none;
+  margin: 0;
+  height: 40px;
 `;
+
+const NumberInput = styled.input`
+  width: 100%;
+  padding: 6px 10px;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+  border-radius: 5px;
+  padding: 5px;
+  display: block;
+  height: 30px;
+  font-size: 20px;
+  font-weight: 400;
+  text-align: center;
+  -webkit-appearance: none;
+  margin: 0;
+  height: 40px;
+`
+
+const AddButton = styled.div`
+  border-radius: 5px;
+  background-color: green;
+  flex: 1;
+  color: white;
+  text-align: center;
+  vertical-align: middle;
+`
 
 const AgeCategoryContainer = styled.div`
   flex: 1;
   text-align: center;
   display: flex;
-  flex-direction: column;
   gap: 5px;
+  flex-direction: column;
 `
 
 const CategoryContainer = styled.div`
   flex: 2;
+  gap: 5px;
   text-align: center;
+  display: flex;
+  flex-direction: column;
 `
 
 const CategoryTitle = styled.h3`
@@ -95,6 +171,7 @@ const CategoryTitle = styled.h3`
 const FieldsContainer = styled.div`
   display: flex;
   flex-direction: row;
+  gap: 15px;
 `
 
 const FormButton = styled.div`
