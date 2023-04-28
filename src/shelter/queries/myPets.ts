4134884f35b7c8c3bd37ca5401ab1e27b9bf6cea
Kamil Kubiak <01161372@pw.edu.pl>
2023-04-28 14:55:00 +0200

    Zmiana z mockowanych danych

--- src/shelter/queries/myPets.ts
+++ src/shelter/queries/myPets.ts
@@ -25,6 +25,7 @@ export const useMyPets = () => {
             {
                 name: "fifik1",
                 breed: "Best Breed",
+                sex: "Female",
                 species: "Best species",
                 description: "",
                 birthday: new Date("12.04.2001"),
@@ -32,26 +33,29 @@ export const useMyPets = () => {
             },
             {
         name: "fifik2",
-        breed: "Best Breed",
+                breed: "Best Breed",
+                sex: "Male",
         species: "Best species",
                 description: "",
-                birthday: new Date("12.04.2001"),
+                birthday: new Date("12.04.2011"),
         id: "2111-111-11111"
       },
       {
         name: "fifik3",
-        breed: "Best Breed",
+          breed: "Best Breed",
+          sex: "Unknown",
         species: "Best species",
           description: "",
-          birthday: new Date("12.04.2001"),
+          birthday: new Date("12.04.2021"),
         id: "3111-111-11111"
       },
       {
         name: "fifik4",
-        breed: "Best Breed",
+          breed: "Best Breed",
+          sex: "Does not apply",
         species: "Best species",
           description: "",
-          birthday: new Date("12.04.2001"),
+          birthday: new Date("12.04.2022"),
         id: "4111-111-11111"
       },
     ] as Pet[]
